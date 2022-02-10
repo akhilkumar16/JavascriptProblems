2f462b4e1c27b0a60da2b5fb5df6fc7a8b1119cf
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-10 13:50:53 +0530

    [Akhil] uc5

--- Problems.js
+++ Problems.js
@@ -21,3 +21,21 @@ for (let i = 0; i < 5; i++)
 }  
 console.log('Sum of 5 two digits number is = ' + sum);
 console.log('Average of the sum of the 5 two digits number is = ' + sum/5);
+//UC5//
+//inch to feet//
+let inchs = 42;
+let ft = inchs / 12;
+console.log("42 in = " + ft + " feet");
+//Rectangular//
+let len = 60; 
+let breadth = 40 
+//Rectangle area//
+let area = (len*breadth);
+let areas = (area * 0.0929).toFixed(2);
+console.log('area in sq metres = ' + areas + ' sq-m');
+//25 plots//
+let totalArea = areas*25;
+console.log('Total Area of 25 such plots in sq-m = ' + totalArea + " sq-m" );
+//area of 25 such plots//
+let areaInAcre = (totalArea / 4047).toFixed(2);
+console.log('Total Area of 25 such plots in acre = ' + areaInAcre +' acres');
