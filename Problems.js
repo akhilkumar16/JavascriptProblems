f2db38fe9f86fec3db933e496a549d1cf842b85e
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-10 13:43:00 +0530

    [Akhil] uc1

--- Problems.js
+++ Problems.js
@@ -1,3 +1,3 @@
 //uc1// Random Numbers
 let Random_number = Math.floor(Math.random() * 10); 
-console.log("Random Number is: " + Random_number); 
+console.log("Random Number is: " + Random_number);  
